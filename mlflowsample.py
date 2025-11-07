8a2990c71e01fb4341048be59e1f9eceb7c3dc25
Nico Moccagatta <nicomoccagatta@gmail.com>
2025-11-07 20:51:33 -0300

    Fix lint2?

--- mlflowsample.py
+++ mlflowsample.py
@@ -19,8 +19,8 @@ import time
 import subprocess
 import os
 
-def entrenar_modelo_iris():
 
+def entrenar_modelo_iris():
 
     mlflow.set_experiment("clasificacion_iris_detallada")
     iris = load_iris()
